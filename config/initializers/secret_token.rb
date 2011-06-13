45a4c20d190764f8962f2c708515165c71128af7
Sohee Whang <sohee.whang@headflow.net>
2011-06-13 12:33:13 +0900

    all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wisdomkit::Application.config.secret_token = 'c5f82bd062e3888a351debb4a37e738c7d339f5e1e67ad26f6d649fbcc951d185ca7361e54d7cd83662f0ebbfcf3c700167bbc49a15fb0918bf5a20571af584f'
